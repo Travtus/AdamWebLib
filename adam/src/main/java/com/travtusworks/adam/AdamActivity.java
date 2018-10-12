aaa9a17f90611dfbc59cf027625252a883727fc9
teodora <theo1208@gmail.com>
2018-10-12 13:30:45 +0300

    updated the link; added progress bar;

--- adam/src/main/java/com/travtusworks/adam/AdamActivity.java
+++ adam/src/main/java/com/travtusworks/adam/AdamActivity.java
@@ -1,10 +1,15 @@
 package com.travtusworks.adam;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by teodora on 14.06.2018.
@@ -14,7 +19,8 @@ public class AdamActivity extends AppCompatActivity{
 
     private static final String TAG = "AdamActivity";
 
-
+    private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,9 +28,56 @@ public class AdamActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_adam);
 
-        WebView webView = findViewById(R.id.webview);
-        webView.loadUrl("https://www.google.com/");
+        webView = findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progress_bar);
 
+        String url = "http://adamwidget.travtus.com/";
+
+        webView.setWebViewClient(new InnerWebViewClient());
+        webView.loadUrl(url);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+    }
+
+    private class InnerWebViewClient extends WebViewClient {
+
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            try {
+                Animation fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_splash);
+                webView.startAnimation(fadeIn);
+
+                fadeIn.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                        progressBar.setVisibility(View.GONE);
+                        webView.setVisibility(View.VISIBLE);
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            } catch (Exception e) {}
+
+        }
     }
 
 }
